778487f436b7518ba759b65bdea48b2d764d4547
hania <h.serafin@student.maastrichtuniversity.nl>
2026-05-02 13:25:16 +0200

    fix of alertStartegy

--- src/main/java/com/alerts/alert_strategies/AlertStrategy.java
+++ src/main/java/com/alerts/alert_strategies/AlertStrategy.java
@@ -1,5 +1,9 @@
 package com.alerts.alert_strategies;
 
+import com.data_management.Patient;
+
 public interface  AlertStrategy {
-    public boolean checkAlert();
+     // null if there is no alert, otherwise string for each category
+    String checkAlert (Patient patient);
 }
+
