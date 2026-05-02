c9eb7ad5c87c0b4311f439564d29e636be91bd56
hania <h.serafin@student.maastrichtuniversity.nl>
2026-05-02 14:17:17 +0200

    work in progress 2

--- src/main/java/com/alerts/alert_strategies/BloodOxygenAlertStrategy.java
+++ src/main/java/com/alerts/alert_strategies/BloodOxygenAlertStrategy.java
@@ -3,22 +3,27 @@ package com.alerts.alert_strategies;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.alerts.AlertGenerator;
+import com.alerts.BasicAlert;
+import com.alerts.alert_factory.AlertFactory;
+import com.alerts.alert_factory.BloodOxygenAlertFactory;
 import com.data_management.Patient;
 import com.data_management.PatientRecord;
 
 public class BloodOxygenAlertStrategy implements AlertStrategy{
     @Override 
-    public String checkAlert(Patient patient){
+    public BasicAlert checkAlert(Patient patient){
         List<PatientRecord> records = patient.getRecords(0, Long.MAX_VALUE);
         List<Double> spo2History = new ArrayList<>();
         for (PatientRecord record : records) {
+            long timeStamp = record.getTimestamp();
             String type = record.getRecordType();
             double value = record.getMeasurementValue();
 
             if (type.equals("Saturation")) {
                  spo2History.add(value);
                 if (value < 92) {
-                    return "urgent: saturation out of range!! value: " + value + "%";
+                    return BloodOxygenAlertFactory.createAlert(patient.getPatientID(), type, timeStamp) ;
                 }   
             }
             if (spo2History.size() > 1) {
