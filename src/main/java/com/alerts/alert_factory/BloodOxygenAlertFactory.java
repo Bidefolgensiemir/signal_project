521a84cc1851a4ff523fa5b69dad8ac11cee8960
k.nitschke <k.nitschke@student.maastrichtuniversity.nl>
2026-05-02 13:00:14 +0200

    Added Individual Factory Subclassses

--- src/main/java/com/alerts/alert_factory/BloodOxygenAlertFactory.java
+++ src/main/java/com/alerts/alert_factory/BloodOxygenAlertFactory.java
@@ -1,11 +1,12 @@
 package com.alerts.alert_factory;
 
 import com.alerts.Alert;
+import com.alerts.BloodOxygenAlert;
 
 public class BloodOxygenAlertFactory extends AlertFactory {
     @Override
     public Alert createAlert(String patientId, String condition, long timeStamp) {
-        return new Alert(patientId, condition, timeStamp);
+        return new BloodOxygenAlert(patientId, condition, timeStamp);
     }
 
 }
